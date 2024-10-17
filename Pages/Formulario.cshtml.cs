fc1eaa5936769f304fc5f009b41a87fc46c2f144
asv091197 <sanchez.vanegas.alexiscbtis44@gmail.com>
2024-10-16 22:59:56 -0600

    Agregar archivos de proyecto.

---
+++ Pages/Formulario.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Formulario.Pages
+{
+    public class FormularioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
